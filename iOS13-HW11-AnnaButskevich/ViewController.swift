df6c895bd3f4a8ed57006bc4c8d5591e34e9c3e7
anabuts <ana.butskevich@gmail.com>
2024-06-13 22:15:09 +0300

    Add items and constraints

--- iOS13-HW11-AnnaButskevich/ViewController.swift
+++ iOS13-HW11-AnnaButskevich/ViewController.swift
@@ -8,15 +8,23 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    // MARK: - Outlets
+    // MARK: - UI
+
+    private lazy var imageView: UIImageView = {
+        let image = UIImage(named: "background image")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "Login"
         label.textColor = .white
-        label.font = UIFont.boldSystemFont(ofSize: 36)
+        label.font = UIFont.boldSystemFont(ofSize: 32)
         label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
@@ -24,36 +32,147 @@ class ViewController: UIViewController {
 
     private lazy var nameTextField: UITextField = {
         let textField = UITextField()
-        textField.textColor = .systemGray
+        textField.font = .systemFont(ofSize: 16)
+        textField.textColor = .black
         textField.textAlignment = .left
         textField.placeholder = "Name"
         textField.backgroundColor = .white
         textField.layer.cornerRadius = 25
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.setLeftIcon(UIImage(named: "user") ?? UIImage.remove)
         return textField
     }()
 
     private lazy var passwordTextField: UITextField = {
         let textField = UITextField()
-        textField.textColor = .systemGray
+        textField.font = .systemFont(ofSize: 16)
+        textField.textColor = .black
         textField.textAlignment = .left
         textField.placeholder = "Password"
         textField.backgroundColor = .white
         textField.layer.cornerRadius = 25
         textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.setLeftIcon(UIImage(named: "lock") ?? UIImage.remove)
         return textField
     }()
 
-    private lazy var button: UIButton = {
-        let button = UIButton()
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(type: .system)
         button.setTitle("Login", for: .normal)
         button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .regular)
         button.clipsToBounds = true
         button.layer.cornerRadius = 25
         button.backgroundColor = .systemBlue
         button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.2
+        button.layer.shadowOffset = .zero
+        button.layer.masksToBounds = false
+        button.layer.shadowRadius = 8
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
         return button
+    }()
+
+    private lazy var forgotPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Forgot your password?", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .regular)
+        button.clipsToBounds = true
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var networksLabel: UILabel = {
+        let textLabel = UILabel()
+        textLabel.text = "or connect with"
+        textLabel.textColor = .white
+        textLabel.font = UIFont.systemFont(ofSize: 16)
+        textLabel.textAlignment = .center
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        return textLabel
+    }()
+    
+    private lazy var facebookButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Facebook", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 20
+        button.backgroundColor = .systemGray
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.2
+        button.layer.shadowOffset = .zero
+        button.layer.masksToBounds = false
+        button.layer.shadowRadius = 8
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
+        return button
+    }()
+
+    private lazy var twitterButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Twitter", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 20
+        button.backgroundColor = .systemGray
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.2
+        button.layer.shadowOffset = .zero
+        button.layer.masksToBounds = false
+        button.layer.shadowRadius = 8
+        button.layer.shouldRasterize = true
+        button.layer.rasterizationScale = UIScreen.main.scale
+        return button
+    }()
+
+    private lazy var networkHorizantalStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [facebookButton, twitterButton])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 20
+        return stack
+    }()
+
+    private lazy var createAcountLabel: UILabel = {
+        let textLabel = UILabel()
+        textLabel.text = "Don't have account?"
+        textLabel.textColor = .white
+        textLabel.font = UIFont.systemFont(ofSize: 16)
+        textLabel.textAlignment = .center
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        return textLabel
+    }()
+
+    private lazy var signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign up", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .medium)
+        button.backgroundColor = .clear
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private lazy var signUpHorizontalStack: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [createAcountLabel, signUpButton])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.distribution = .fillProportionally
+        stack.spacing = 10
+        return stack
     }()
 
     // MARK: - Lifecycle
@@ -66,44 +185,68 @@ class ViewController: UIViewController {
     // MARK: - Setup
 
     private func setupHierarchy() {
-        view.addSubview(titleLabel)
-        view.addSubview(nameTextField)
-        view.addSubview(passwordTextField)
-        view.addSubview(button)
+        [imageView, titleLabel, nameTextField, passwordTextField, loginButton, forgotPasswordButton, networksLabel, networkHorizantalStack, signUpHorizontalStack].forEach {view.addSubview($0)
+        }
     }
 
     private func setupLayout() {
 
+        imageView.snp.makeConstraints {make in
+            make.center.equalTo(view)
+            make.width.equalTo(view)
+            make.height.equalTo(view)
+        }
+
         titleLabel.snp.makeConstraints {make in
-            make.centerX.equalTo(view)
-            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(100)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(80)
+            make.centerX.equalToSuperview()
         }
 
         nameTextField.snp.makeConstraints {make in
-            make.center.equalTo(view)
-            make.top.equalTo(titleLabel.snp.bottom).offset(20)
-            make.leading.equalToSuperview().offset(40)
-            make.bottom.equalTo(passwordTextField.snp.top).offset(-20)
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
+            make.left.right.equalToSuperview().inset(40)
             make.height.equalTo(50)
         }
 
         passwordTextField.snp.makeConstraints {make in
-            make.center.equalTo(view)
-            make.leading.equalToSuperview().offset(40)
+            make.top.equalTo(nameTextField.snp.bottom).offset(20)
+            make.left.right.equalToSuperview().inset(40)
             make.height.equalTo(50)
         }
 
-        button.snp.makeConstraints {make in
-            make.centerX.equalTo(view)
-            make.leading.equalToSuperview().offset(40)
+        loginButton.snp.makeConstraints {make in
             make.top.equalTo(passwordTextField.snp.bottom).offset(60)
+            make.left.right.equalToSuperview().inset(40)
             make.height.equalTo(50)
+        }
+
+        forgotPasswordButton.snp.makeConstraints {make in
+            make.top.equalTo(loginButton.snp.bottom).offset(10)
+            make.left.right.equalToSuperview().inset(40)
+            make.height.equalTo(50)
+        }
+
+        networksLabel.snp.makeConstraints {make in
+            make.top.equalTo(forgotPasswordButton.snp.bottom).offset(150)
+            make.left.right.equalToSuperview().inset(40)
+        }
+
+        networkHorizantalStack.snp.makeConstraints {make in
+            make.top.equalTo(networksLabel.snp.bottom).offset(20)
+            make.width.equalTo(200)
+            make.height.equalTo(40)
+            make.centerX.equalToSuperview()
+        }
+
+        signUpHorizontalStack.snp.makeConstraints {make in
+            make.top.equalTo(networkHorizantalStack.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
         }
     }
 
     // MARK: - Action
 
     @objc private func buttonPressed() {
-
     }
+        
 }
